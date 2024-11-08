3027d0c744bbcf76305e8d5d50f674d29379870b
Emily502 <emilygithubprogramacao@gmail.com>
2024-11-08 10:36:11 +0000

    123

--- graficos/informacaesglobais.js
+++ graficos/informacaesglobais.js
@@ -5,8 +5,3 @@ async function vizualizarinformacaesglobais() {
  console.log(dados)
  const paragrafo = document.createElement('p')
 }
-{
-    "total_pessoas_conectadas": 5.04e9
-    "tempo_medio": 2.38
-    "total_pessoas_mundo": 7.888e9
-}
