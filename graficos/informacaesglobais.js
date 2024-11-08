0002a71ec44c12eeec49d7fefa935905d39140f9
Emily502 <emilygithubprogramacao@gmail.com>
2024-11-08 10:40:53 +0000

    123

--- graficos/informacaesglobais.js
+++ graficos/informacaesglobais.js
@@ -1,7 +1,2 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-async function vizualizarinformacaesglobais() {
- const res = await fetch(url)
- const dados = await res.json()   
- console.log(dados)
- const paragrafo = document.createElement('p')
-}
+console.log(url)
