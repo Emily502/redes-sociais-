45a6ab00ae4812c192c04c1c9ecc62c1be8a0be2
Emily502 <emilygithubprogramacao@gmail.com>
2024-11-29 10:58:28 +0000

    123

--- graficosescola/informacoes-escola.js
+++ graficosescola/informacoes-escola.js
@@ -1,1 +1,13 @@
 const url = https//raw.githubusercontent.com/Emily502/redes-sociais-/refs/heads/main/graficos/informaconhesescola.json
+
+async function vizualizarinformacaesglobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)   
+    console.log(dados)
+    paragrafo.classlist.add('graficos-conterner-texto')  
+   paragrafo.inneHTML = `Diacordo com pesquisas realizadas ${dados.toral-de-pessoas-no-mundo} de pessoasusam redes sociais e no colegio CCM Vinicios de Morais X alunos paraticiparam `        
+       
+    
+   }
+    vizualizarinformacaesglobais()
